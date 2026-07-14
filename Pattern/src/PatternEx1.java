7111bbdb0857a26e2399f133b2c74084514150cb
pratima patel <pratimapatel691@gmail.com>
2026-07-14 14:11:46 +0530

    Pattern

---
+++ Pattern/src/PatternEx1.java
@@ -0,0 +1,36 @@
+
+public class PatternEx1 {
+public static void main(String[] args) {
+	//java.util.Scanner sc=new java.util.Scanner(System.in);
+//	int n=sc.nextInt();
+	
+	int h=5;
+	for(int b=1;b<=5;b++) {
+		for(int a=1;a<=h;a++) {
+		System.out.print("*");
+		
+	}
+	System.out.println();
+	h--;
+}
+	
+	int n=1;
+	for(int b=1;b<=5;b++) {
+		for(int a=1;a<=n;a++) {
+		System.out.print("*");
+		
+	}
+	System.out.println();
+	n++;
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+
